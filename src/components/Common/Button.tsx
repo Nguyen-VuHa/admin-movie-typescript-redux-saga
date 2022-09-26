4d068faedc42f646b257e5c8275e5a07f79e61e7
Nguyen Vu Ha <havu20111999@gmail.com>
2022-09-26 22:06:49 +0700

    update

--- src/components/Common/Button.tsx
+++ src/components/Common/Button.tsx
@@ -13,7 +13,7 @@ interface ButtonProps {
     loadingText?: string,
     style?: Object,
     title?: string,
-    buttonType?: 'default' | 'dismiss' | 'disable' | ''
+    buttonType?: 'default' | 'dismiss' | 'disable'
 }
 
 export function Button({ children, className, onClick, loading = false, loadingText, style, title, buttonType = 'default' }: ButtonProps) {
@@ -27,7 +27,7 @@ export function Button({ children, className, onClick, loading = false, loadingT
     return (
         <button
             ref={buttonRef}
-            className={cx('button-layout', className, buttonType !== 'default' ? buttonType : '')}
+            className={cx('button-layout', className, buttonType)}
             type="button"
             onClick={() => {
                 if(!loading)
